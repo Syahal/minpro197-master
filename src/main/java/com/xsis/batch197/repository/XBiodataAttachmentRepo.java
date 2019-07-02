bac4f4b41ea8ff7a5861ac0fd5c49fd58b5477d0
Kris Sivam <krissivam@gmail.com>
2019-07-02 09:38:45 +0700

    add Dokumen

--- src/main/java/com/xsis/batch197/repository/XBiodataAttachmentRepo.java
+++ src/main/java/com/xsis/batch197/repository/XBiodataAttachmentRepo.java
@@ -1,5 +1,7 @@
 package com.xsis.batch197.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,6 @@ import com.xsis.batch197.model.XBiodataAttachmentModel;
 
 @Repository
 public interface XBiodataAttachmentRepo extends JpaRepository<XBiodataAttachmentModel, Long> {
-
+	
+	List<XBiodataAttachmentModel> findByBiodataId(Long biodataId);
 }
